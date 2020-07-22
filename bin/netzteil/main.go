01aca86e0326a5b686cebbed1fecd455d7aa0cda
Stefan Tatschner <stefan@rumpelsepp.org>
2020-07-22 10:02:44 +0200

    Implement get/set current/voltage for client (dragons!)

--- bin/netzteil/main.go
+++ bin/netzteil/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -59,6 +60,8 @@ func (c *netzteilClient) getDeviceList() ([]string, error) {
 	}
 	return parsedResp, nil
 }
+
+// TODO: find a way to unify getter/setter
 
 func (c *netzteilClient) setOutParam(device, channel uint, state bool) error {
 	var (
@@ -126,20 +129,116 @@ func (c *netzteilClient) getOutParam(device, channel uint) (bool, error) {
 	return parsedResp, nil
 }
 
-func (c *netzteilClient) setChannel(device uint, channel uint, state bool) error {
-	return c.setOutParam(device, channel, state)
+func (c *netzteilClient) getVoltage(device uint, channel uint) (float64, error) {
+	var (
+		uri        = *c.baseURL
+		reqPath    string
+		parsedResp float64
+	)
+	reqPath = fmt.Sprintf("/_netzteil/api/devices/%d/channels/%d/voltage", device, channel)
+	uri.Path = path.Join(uri.Path, reqPath)
+	resp, err := c.client.Get(uri.String())
+	if err != nil {
+		return 0, err
+	}
+	if err := recvJSON(resp, &parsedResp); err != nil {
+		return 0, err
+	}
+	return parsedResp, nil
 }
 
-func (c *netzteilClient) setMaster(device uint, state bool) error {
-	return c.setOutParam(device, 0, state)
+func (c *netzteilClient) setVoltage(device uint, channel uint, voltage float64) error {
+	var (
+		uri     = *c.baseURL
+		reqPath string
+		body    []byte
+	)
+	reqPath = fmt.Sprintf("/_netzteil/api/devices/%d/channels/%d/voltage", device, channel)
+	uri.Path = path.Join(uri.Path, reqPath)
+	body, _ = json.Marshal(voltage)
+	req, err := http.NewRequest(http.MethodPut, uri.String(), bytes.NewReader(body))
+	if err != nil {
+		return err
+	}
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return err
+	}
+	logger.LogDebug(resp)
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logger.LogError(err)
+		logger.LogError(resp)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		logger.LogErrorf(string(respBody))
+	}
+	return nil
+}
+
+func (c *netzteilClient) setCurrent(device uint, channel uint, current float64) error {
+	var (
+		uri     = *c.baseURL
+		reqPath string
+		body    []byte
+	)
+	reqPath = fmt.Sprintf("/_netzteil/api/devices/%d/channels/%d/current", device, channel)
+	uri.Path = path.Join(uri.Path, reqPath)
+	body, _ = json.Marshal(current)
+	req, err := http.NewRequest(http.MethodPut, uri.String(), bytes.NewReader(body))
+	if err != nil {
+		return err
+	}
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return err
+	}
+	logger.LogDebug(resp)
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logger.LogError(err)
+		logger.LogError(resp)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		logger.LogErrorf(string(respBody))
+	}
+	return nil
+}
+
+func (c *netzteilClient) getCurrent(device uint, channel uint) (float64, error) {
+	var (
+		uri        = *c.baseURL
+		reqPath    string
+		parsedResp float64
+	)
+	reqPath = fmt.Sprintf("/_netzteil/api/devices/%d/channels/%d/current", device, channel)
+	uri.Path = path.Join(uri.Path, reqPath)
+	resp, err := c.client.Get(uri.String())
+	if err != nil {
+		return 0, err
+	}
+	if err := recvJSON(resp, &parsedResp); err != nil {
+		return 0, err
+	}
+	return parsedResp, nil
 }
 
 func (c *netzteilClient) getChannel(device uint, channel uint) (bool, error) {
 	return c.getOutParam(device, channel)
 }
 
+func (c *netzteilClient) setChannel(device uint, channel uint, state bool) error {
+	return c.setOutParam(device, channel, state)
+}
+
 func (c *netzteilClient) getMaster(device uint) (bool, error) {
 	return c.getOutParam(device, 0)
+}
+
+func (c *netzteilClient) setMaster(device uint, state bool) error {
+	return c.setOutParam(device, 0, state)
 }
 
 func main() {
@@ -192,6 +291,46 @@ func main() {
 				device = "NO DESCRIPTION"
 			}
 			fmt.Printf("%02d: %s\n", i+1, device)
+		}
+	case "voltage":
+		switch *op {
+		case operationGET:
+			voltage, err := client.getVoltage(*device, *channel)
+			if err != nil {
+				logger.LogCritical(err)
+				os.Exit(1)
+			}
+			fmt.Println(voltage)
+		case operationSET:
+			arg, err := strconv.ParseFloat(*opArg, 32)
+			if err != nil {
+				logger.LogCritical(err)
+				os.Exit(1)
+			}
+			if err := client.setVoltage(*device, *channel, arg); err != nil {
+				logger.LogCritical(err)
+				os.Exit(1)
+			}
+		}
+	case "current":
+		switch *op {
+		case operationGET:
+			current, err := client.getCurrent(*device, *channel)
+			if err != nil {
+				logger.LogCritical(err)
+				os.Exit(1)
+			}
+			fmt.Println(current)
+		case operationSET:
+			arg, err := strconv.ParseFloat(*opArg, 32)
+			if err != nil {
+				logger.LogCritical(err)
+				os.Exit(1)
+			}
+			if err := client.setCurrent(*device, *channel, arg); err != nil {
+				logger.LogCritical(err)
+				os.Exit(1)
+			}
 		}
 	case "out":
 		switch *op {
